c6c2dd7d9e43727e61eb21ff5cbf34f6d5bf126e
HSATAN <2499090390@qq.com>
2017-06-23 20:02:46 +0800

    add schedule

--- django-web/intelligence_spider/intelligence_spider/config.py
+++ django-web/intelligence_spider/intelligence_spider/config.py
@@ -1,3 +1,13 @@
 #_*_ coding:utf-8 _*_
+from datetime import timedelta
 celery_result_backend='redis://47.93.24.159:6379/1'
 broker_url='redis://47.93.24.159:6379/0'
+
+celerybeat_schedule={
+    'every-5-min':{
+        'task':'tasks.spider',
+        'schedule':timedelta(seconds=30),
+        'args':('weather')
+
+    }
+}
