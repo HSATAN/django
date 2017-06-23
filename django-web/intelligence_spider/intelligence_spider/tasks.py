67e059284ddbcdc8a2ce66d53233d20374d8e76e
HSATAN <2499090390@qq.com>
2017-06-23 09:56:36 +0800

    fix celery

--- django-web/intelligence_spider/intelligence_spider/tasks.py
+++ django-web/intelligence_spider/intelligence_spider/tasks.py
@@ -1,6 +1,7 @@
 from celery import Celery
 
-app=Celery('wether',broker='redis://47.93.24.159:6379/0',backend='redis://47.93.24.159:6379/0')
+app=Celery('wether')
+app.config_from_object('config')
 @app.task()
 def hello(name):
     return 'hello '+name
