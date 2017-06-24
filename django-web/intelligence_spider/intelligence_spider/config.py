a5c227ce4fedc9524b3f4578f79a06f855940bfd
edison <2499090390@qq.com>
2017-06-24 23:03:21 +0800

    add crontab

--- django-web/intelligence_spider/intelligence_spider/config.py
+++ django-web/intelligence_spider/intelligence_spider/config.py
@@ -1,13 +1,14 @@
 #_*_ coding:utf-8 _*_
 from datetime import timedelta
+from celery.schedules import crontab
 CELERY_RESULT_BACKEND='redis://47.93.24.159:6379/1'
-BROKER_URL='redis://47.93.24.159:6379/0'
+BROKER_URL='redis://47.93.24.159:6379/1'
 
 CELERYBEAT_SCHEDULE={
     'every-5-min':{
         'task':'tasks.spider',
-        'schedule':timedelta(seconds=30),
-        'args':('weather')
+        'schedule':crontab(hour=6,minute=10),
+        'args':None
 
     }
 }
--- django-web/intelligence_spider/intelligence_spider/tasks.py
+++ django-web/intelligence_spider/intelligence_spider/tasks.py
@@ -3,7 +3,7 @@ import subprocess
 app=Celery()
 app.config_from_object('config')
 @app.task()
-def spider(name):
-    crawl = subprocess.Popen("scrapy crawl %s" % spider, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+def spider():
+    crawl = subprocess.Popen("scrapy crawl weather", stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
     crawl.communicate()
-    return 'run  '+name
+    return 'run  '+'wether'
