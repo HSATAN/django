4bac50166250a4400bfb919c35f8ac8262bdec82
edison <2499090390@qq.com>
2017-06-24 21:55:11 +0800

    fix celery

--- django-web/intelligence_spider/intelligence_spider/config.py
+++ django-web/intelligence_spider/intelligence_spider/config.py
@@ -1,9 +1,9 @@
 #_*_ coding:utf-8 _*_
 from datetime import timedelta
-celery_result_backend='redis://47.93.24.159:6379/1'
-broker_url='redis://47.93.24.159:6379/0'
+CELERY_RESULT_BACKEND='redis://47.93.24.159:6379/1'
+BROKER_URL='redis://47.93.24.159:6379/0'
 
-celerybeat_schedule={
+CELERYBEAT_SCHEDULE={
     'every-5-min':{
         'task':'tasks.spider',
         'schedule':timedelta(seconds=30),
