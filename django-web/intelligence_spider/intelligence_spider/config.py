dac50110f371acd2427ab562a1095582b9a4e619
edison <2499090390@qq.com>
2017-06-24 23:32:20 +0800

    fix celery

--- django-web/intelligence_spider/intelligence_spider/config.py
+++ django-web/intelligence_spider/intelligence_spider/config.py
@@ -1,13 +1,19 @@
 #_*_ coding:utf-8 _*_
 from datetime import timedelta
 from celery.schedules import crontab
-CELERY_RESULT_BACKEND='redis://47.93.24.159:6379/1'
+#CELERY_RESULT_BACKEND='redis://47.93.24.159:6379/1'
 BROKER_URL='redis://47.93.24.159:6379/1'
-
+CELERY_RESULT_BACKEND='mongodb'
+CELERY_RESULT_BACKEND_SETTINGS={
+    'host':'47.93.5.189',
+    'port':27017,
+    'database':'spidertask',
+    'taskmeta_collection':'spidermeta'
+}
 CELERYBEAT_SCHEDULE={
     'every-5-min':{
         'task':'tasks.spider',
-        'schedule':crontab(hour=6,minute=10),
+        'schedule':timedelta(seconds=60),
         'args':None
 
     }
