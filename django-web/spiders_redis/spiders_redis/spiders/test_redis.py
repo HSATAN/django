b5494053e238169454ada15f0b44714439ec4965
HSATAN <2499090390@qq.com>
2017-07-14 17:46:57 +0800

    添加redis_key

--- django-web/spiders_redis/spiders_redis/spiders/test_redis.py
+++ django-web/spiders_redis/spiders_redis/spiders/test_redis.py
@@ -3,6 +3,7 @@ from scrapy_redis.spiders import RedisSpider
 
 class MyRSpider(RedisSpider):
     name = 'myspider'
+    redis_key = 'myspider:test'
     def parse(self, response):
         print(response.text)
         pass
